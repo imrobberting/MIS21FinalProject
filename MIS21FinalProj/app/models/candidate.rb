a46503a66ea9ce3aa2fba1f55a304b873e434463
harvli1216 <harvli1216@gmail.com>
2015-07-14 22:47:36 +0800

    Model validation
    validates uniqueness and presence

--- MIS21FinalProj/app/models/candidate.rb
+++ MIS21FinalProj/app/models/candidate.rb
@@ -1,3 +1,6 @@
 class Candidate < ActiveRecord::Base
   belongs_to :position
+  has_many :votes
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
 end
